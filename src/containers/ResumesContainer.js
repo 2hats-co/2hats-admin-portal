c75a57979ee1e009a45091d6585f3c6897a6f6d5
shams mosowi <shams.mosowi@gmail.com>
2018-10-19 14:42:26 +1100

    feedback form section

--- src/containers/ResumesContainer.js
+++ src/containers/ResumesContainer.js
@@ -3,6 +3,37 @@ import {withNavigation} from '../components/withNavigation'
 import CandidatesList from '../components/Resumes/CandidatesList'
 import Grid from '@material-ui/core/Grid'
 import Submission from '../components/Submission'
+import FeedbackForm from '../components/FeedbackForm';
+const sections = [{name:'Resume',
+                   options:[{label:'Clear link between resume/job applied for',
+                            content:'Your resume shows a clear link between the industry you’re applying for, your experience and your interests - well done.',
+                            isPositive:true
+                            },
+                            {label:'Resume shows no clear interest in the job',
+                            content:'Your resume shows a clear link between the industry you’re applying for, your experience and your interests - well done.',
+                            isPositive:false
+                            },
+                            {label:'Resume lacks required information',
+                            content:'Your resume shows a clear link between the industry you’re applying for, your experience and your interests - well done.',
+                            isPositive:false
+                            },
+                            ]
+                    },{name:'Skills',
+                    options:[{label:'Clear link between resume/job applied for',
+                             content:'Your resume shows a clear link between the industry you’re applying for, your experience and your interests - well done.',
+                             isPositive:true
+                             },
+                             {label:'Resume shows no clear interest in the job',
+                             content:'Your resume shows a clear link between the industry you’re applying for, your experience and your interests - well done.',
+                             isPositive:false
+                             },
+                             {label:'Resume lacks required information',
+                             content:'Your resume shows a clear link between the industry you’re applying for, your experience and your interests - well done.',
+                             isPositive:false
+                             },
+                             ]
+                     }
+                ]
 class ResumeContainer extends Component{
     constructor(props){
         super(props)
@@ -19,11 +50,14 @@ class ResumeContainer extends Component{
     }
     render(){
         return(<Grid container direction='row'>
-        <Grid item xs={3}>
-            <CandidatesList setCandidate={this.setCandidate}/>
+            <Grid item xs={3}>
+                 <CandidatesList setCandidate={this.setCandidate}/>
             </Grid>
-            <Grid item xs={9}>
-            <Submission UID={this.state.candidateUID}/>
+            <Grid item xs={6}>
+                 <Submission UID={this.state.candidateUID}/>
+            </Grid>
+            <Grid item xs={3}>
+                 <FeedbackForm sections={sections}/>
             </Grid>
         </Grid>)
     }
