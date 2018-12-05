7c53d583e9381dbf8a4ff6c9fb7c308893785c3f
Sidney Alcantara <sidney@sidney.me>
2018-12-05 17:41:00 +1100

    remove react-pdf (yes!)

--- src/components/Submission/SubmissionDetails.js
+++ src/components/Submission/SubmissionDetails.js
@@ -1,15 +1,9 @@
 import React, { Component } from 'react';
 
 import {withStyles} from '@material-ui/core/styles';
-import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 import Chip from '@material-ui/core/Chip';
-import Slider from '@material-ui/lab/Slider';
 
-import SizeIcon from '@material-ui/icons/FormatSize';
-import DownloadIcon from '@material-ui/icons/ArrowDownward';
-
-import { Document, Page } from 'react-pdf';
 import EduExpCard from './EduExpCard';
 
 const styles = theme => ({
@@ -22,16 +16,6 @@ const styles = theme => ({
         marginRight: 4,
         marginBottom: 4,
     },
-    pdfDocument: {
-        width: 'calc(100vw - 64px - 400px - 80px)',
-    },
-    pdfPage: {
-        boxShadow: '0 0 0 1px rgba(0,0,0,.1)',
-        marginBottom: 8,
-    },
-    scalable: {
-        transformOrigin: '50% 0',
-    },
     iframe: {
         width: '100%',
         border: 'none',
@@ -40,58 +24,35 @@ const styles = theme => ({
     },
 });
 
-class submissionDetails extends Component {
-    constructor(props) {
-        super(props);
+function submissionDetails(props) {
+    const { submission, classes } = props;
 
-        this.state = { numPages: 0, sliderValue: 1 };
-    }
+    return (<React.Fragment>
+        <Typography className={classes.subheading} variant="subheading">Bio:</Typography>
+        <Typography variant="body1">{submission.submissionContent.bio}</Typography>
 
-    onDocumentLoadSuccess = ({ numPages }) => {
-        this.setState({ numPages });
-    }
+        <Typography className={classes.subheading} variant="subheading">Available Days:</Typography>
+        <Typography variant="body1">{submission.submissionContent.availableDays}</Typography>
 
-    handleSliderChange = (event, sliderValue) => {
-        this.setState({ sliderValue });
-    }
 
-    render() {
-        const { submission, classes } = this.props;
-        const { numPages, sliderValue } = this.state;
+        <Typography className={classes.subheading} variant="subheading">Skills:</Typography>
+        {submission.submissionContent.skills.map(x =>
+            <Chip color="primary" label={x} key={x} className={classes.chip} />
+        )}
 
-        const pages = [];
-        for (let i = 0; i < numPages; i++) {
-            pages.push(<Page pageNumber={i + 1} key={i} width={window.innerWidth - 64 - 400 - 80}
-            className={classes.pdfPage} />);
+        <Typography className={classes.subheading} variant="subheading">{submission.submissionContent.process === 'upload' ?'Resume':'Profile'}:</Typography>
+        {submission.submissionContent.process === 'upload' && 
+            <iframe src={submission.submissionContent.resumeFile.downloadURL} className={classes.iframe} />
         }
-
-        return (<React.Fragment>
-            <Typography className={classes.subheading} variant="subheading">Bio:</Typography>
-            <Typography variant="body1">{submission.submissionContent.bio}</Typography>
-
-            <Typography className={classes.subheading} variant="subheading">Available Days:</Typography>
-            <Typography variant="body1">{submission.submissionContent.availableDays}</Typography>
-
-
-            <Typography className={classes.subheading} variant="subheading">Skills:</Typography>
-            {submission.submissionContent.skills.map(x =>
-                <Chip color="primary" label={x} key={x} className={classes.chip} />
-            )}
-
-            <Typography className={classes.subheading} variant="subheading">{submission.submissionContent.process === 'upload' ?'Resume':'Profile'}:</Typography>
-            {submission.submissionContent.process === 'upload' && 
-                <iframe src={submission.submissionContent.resumeFile.downloadURL} className={classes.iframe} />
-            }
-            {submission.submissionContent.process === 'build' && 
-            <div>
-            <Typography className={classes.subheading} variant="subheading">Education:</Typography>
-            {submission.submissionContent.education.map((x, i) => <EduExpCard key={i} title={x.degree} label={x.major} description={x.description} startDate={x.startDate} endDate={x.endDate}/>)}
-            <Typography className={classes.subheading} variant="subheading">Experience:</Typography>
-            {submission.submissionContent.experience.map((x, i) => <EduExpCard key={i} title={`${x.title} - ${x.type}`} label={x.organisation} description={x.description} startDate={x.startDate} endDate={x.endDate}/>)}
-            </div>
-            }
-        </React.Fragment>);
-    }
+        {submission.submissionContent.process === 'build' && 
+        <div>
+        <Typography className={classes.subheading} variant="subheading">Education:</Typography>
+        {submission.submissionContent.education.map((x, i) => <EduExpCard key={i} title={x.degree} label={x.major} description={x.description} startDate={x.startDate} endDate={x.endDate}/>)}
+        <Typography className={classes.subheading} variant="subheading">Experience:</Typography>
+        {submission.submissionContent.experience.map((x, i) => <EduExpCard key={i} title={`${x.title} - ${x.type}`} label={x.organisation} description={x.description} startDate={x.startDate} endDate={x.endDate}/>)}
+        </div>
+        }
+    </React.Fragment>);
 }
 
 export default withStyles(styles)(submissionDetails);
