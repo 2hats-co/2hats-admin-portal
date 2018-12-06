625ad3c680817c4f98773d0d8c92cc99dca58843
shams mosowi <shams.mosowi@gmail.com>
2018-12-06 21:59:49 +1100

    new search

---
+++ src/components/Search/index.js
@@ -0,0 +1,56 @@
+import React,{Component} from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
+import TextField  from '@material-ui/core/TextField';
+import { useSearch } from '../../hooks/useSearch';
+
+const styles = theme => ({
+    paginationBar: {
+        padding: '6px 24px',
+        borderBottom: '1px solid rgba(0,0,0,.1)',
+        position: 'relative',
+        '&::after': {
+            content: '""',
+            display: 'block',
+            position: 'absolute',
+            background: 'linear-gradient(to bottom, rgba(0,0,0,.05), rgba(0,0,0,0))',
+            bottom: -21,
+            left: 0,
+            width: '100%',
+            height: 20,
+        },
+    },
+    paginationButtons: {
+        width: 24,
+        height: 24,
+    },
+    subheading: {
+        display: 'inline',
+        verticalAlign: 'middle',
+        color: 'rgba(0,0,0,.53)',
+        fontWeight: 400,
+    },
+});
+
+
+function Search(props){
+    const handleRoute = (hit) => {
+        
+    }
+    const [searchState,searchDispatch] = useSearch()
+    const {results} = searchState
+    console.log('results',results)
+        return(
+        <Grid container direction="column" style={{height:'100%'}}>
+           <TextField
+            onChange={e => { searchDispatch({search:e.target.value})}}
+           />
+           <ul>
+           {results.hits && results.hits.map(hit=> <li onClick={()=>{console.log(hit)}}>{hit.firstName}</li>)}
+           </ul>
+        </Grid>)
+    
+}
+export default withStyles(styles)(Search);
---
+++ src/hooks/useSearch.js
@@ -0,0 +1,31 @@
+import { useEffect, useReducer } from 'react';
+import { ALGOLIA_INDEX, createAlgoliaIndex } from '../config/algolia'
+
+const searchReducer = (prevState, state) => {
+    console.log(prevState,state)
+    return ({...prevState,...state})
+}
+
+const updateQuery = async(index,search,filters,searchDispatch) =>{
+  searchDispatch({prevSearch:search,prevFilters:filters})
+  const results = await index.search(search,{
+    "hitsPerPage":5
+  })
+  searchDispatch({results})
+}
+
+export function useSearch() {
+    const [searchState, searchDispatch] = useReducer(searchReducer,
+         {search: '', filters:[],prevSearch: '', prevFilters:[],results:[]});
+    const index = createAlgoliaIndex(ALGOLIA_INDEX.candidates)
+    useEffect(() => {
+        const {search, prevSearch,filters, prevFilters} = searchState
+        if(search !==prevSearch || filters !== prevFilters ){
+            updateQuery(index,search,filters,searchDispatch)
+        }
+        return () => {
+        };
+    },[searchState]);
+
+    return [searchState, searchDispatch] ;
+}
