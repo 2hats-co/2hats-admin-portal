8c3d65432d3970df009fe58ccc01da51f535a88d
Sidney Alcantara <sidney@sidney.me>
2018-12-12 14:12:26 +1100

    clean up generate-build-id

--- generate-build-id.js
+++ generate-build-id.js
@@ -2,16 +2,13 @@ var fs = require('fs');
 var gitHash = require('git-rev-sync');
 
 console.log('Getting build details...');
-fs.readFile('src/metadata.json', function(err,content) {
-    if (err) throw err;
-    var metadata = JSON.parse(content);
-    metadata.build = metadata.build + 1;
-    metadata.hash = gitHash.short();
-    metadata.date = new Date();
 
-    fs.writeFile('src/metadata.json',JSON.stringify(metadata),function(err){
-        if(err) throw err;
-        console.log('Current build number: ' + metadata.build);
-        console.log('Current build hash: ' + metadata.hash);
-    })
+const metadata = {
+    hash: gitHash.short(),
+    date: new Date(),
+};
+
+fs.writeFile('src/metadata.json',JSON.stringify(metadata),function(err){
+    if(err) throw err;
+    console.log('Current build hash: ' + metadata.hash);
 });
