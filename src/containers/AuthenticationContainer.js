92c82cb3882c6421e02268acdd2d35a28b2474ac
victorhchan <initialvic13@gmail.com>
2018-12-21 17:35:47 +1100

    deploying

--- src/containers/AuthenticationContainer.js
+++ src/containers/AuthenticationContainer.js
@@ -1,71 +1,70 @@
-import React from 'react';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import withStyles from '@material-ui/core/styles/withStyles';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import Slide from '@material-ui/core/Slide';
+import React from "react";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import withStyles from "@material-ui/core/styles/withStyles";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import Slide from "@material-ui/core/Slide";
 
-import LogoInCard from '../components/LogoInCard';
-import GoogleButton from '../components/Auth/GoogleButton';
+import LogoInCard from "../components/LogoInCard";
+import GoogleButton from "../components/Auth/GoogleButton";
 // import { CLOUD_FUNCTIONS, callable } from '../firebase/functions';
-
+//
 const styles = theme => ({
-    root: {
-        height:110
-    },
+  root: {
+    height: 110
+  }
 });
-class AuthenticationContainer extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = { loading: false };
+class AuthenticationContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { loading: false };
 
-        this.handleGoogleButton = this.handleGoogleButton.bind(this);
-    }
-    
-    handleGoogleButton() {
-        this.setState({ loading: true });
-    }
-    render(){
-        const {classes} = this.props;
+    this.handleGoogleButton = this.handleGoogleButton.bind(this);
+  }
 
-        if (this.state.loading) {
-            return(
-                <LogoInCard width={350} height={260}>
-                    <Grid
-                        container
-                        className={classes.root}
-                        alignItems="center"
-                        direction="column"
-                        justify="center"
-                    >
-                        <CircularProgress size={64} />
-                    </Grid>
-                </LogoInCard>
-            );
-        } else {
-            return(
-                <Slide in direction="up">
-                    <LogoInCard width={350} height={260}>
-                        <Grid
-                            container
-                            className={classes.root}
-                            alignItems="center"
-                            direction="column"
-                            justify="space-between"
-                        >
-                            <Typography variant='title'>Admin Portal</Typography>
-                            <GoogleButton
-                                id="google-button"
-                                action="Sign in"
-                                onClick={this.handleGoogleButton}
-                            />
-                            <Typography variant='caption'>Use your 2hats email</Typography>
-                        </Grid>
-                    </LogoInCard>
-                </Slide>
-            );
-        }
-    }
+  handleGoogleButton() {
+    this.setState({ loading: true });
+  }
+  render() {
+    const { classes } = this.props;
 
+    if (this.state.loading) {
+      return (
+        <LogoInCard width={350} height={260}>
+          <Grid
+            container
+            className={classes.root}
+            alignItems="center"
+            direction="column"
+            justify="center"
+          >
+            <CircularProgress size={64} />
+          </Grid>
+        </LogoInCard>
+      );
+    } else {
+      return (
+        <Slide in direction="up">
+          <LogoInCard width={350} height={260}>
+            <Grid
+              container
+              className={classes.root}
+              alignItems="center"
+              direction="column"
+              justify="space-between"
+            >
+              <Typography variant="title">Admin Portal</Typography>
+              <GoogleButton
+                id="google-button"
+                action="Sign in"
+                onClick={this.handleGoogleButton}
+              />
+              <Typography variant="caption">Use your 2hats email</Typography>
+            </Grid>
+          </LogoInCard>
+        </Slide>
+      );
+    }
+  }
 }
 export default withStyles(styles)(AuthenticationContainer);
