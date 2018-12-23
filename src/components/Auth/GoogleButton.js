1b39830885303651e7a53f1c9a643ff5eff3583a
shams mosowi <shams.mosowi@gmail.com>
2018-12-24 09:45:19 +1100

    functional comp google button

--- src/components/Auth/GoogleButton.js
+++ src/components/Auth/GoogleButton.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Button from '@material-ui/core/Button';
 import { GOOGLE_CID_PRODUCTION, GOOGLE_CID_STAGING } from '../../config/auth';
@@ -23,66 +23,54 @@ const styles = theme => ({
     marginRight: 17,
   },
 });
-class GoogleButton extends Component {
-  constructor(props) {
-    super(props);
-    this.handleGoogleAuthFail = this.handleGoogleAuthFail.bind(this);
-    this.handleRouting = this.handleRouting.bind(this);
-    this.getToken = this.getToken.bind(this);
-    if (process.env.REACT_APP_ENV === 'PRODUCTION') {
-      this.state = { cid: GOOGLE_CID_PRODUCTION };
-      console.log('google auth production', GOOGLE_CID_PRODUCTION);
-    } else {
-      this.state = { cid: GOOGLE_CID_STAGING };
-      console.log('google auth staging', GOOGLE_CID_STAGING);
-    }
-  }
+function GoogleButton(props) {
+  const [cid, setCID] = useState(null);
 
-  handleRouting(route) {
-    this.props.history.replace(route);
+  if (process.env.REACT_APP_ENV === 'PRODUCTION' && !cid) {
+    setCID(GOOGLE_CID_PRODUCTION);
+    console.log('google auth production', GOOGLE_CID_PRODUCTION);
+  } else if (!cid) {
+    setCID(GOOGLE_CID_STAGING);
+    console.log('google auth staging', GOOGLE_CID_STAGING);
   }
-  handleGoogleAuthFail = error => {
+  const handleRouting = route => {
+    this.props.history.replace(route);
+  };
+  const handleGoogleAuthFail = error => {
     console.log('google auth fail', error);
   };
 
-  getToken(r) {
-    console.log(r);
-    // if (r.profileObj.email.split("@")[1] === "2hats.com.au") {
-    //   console.log("yay");
-    authAdmin(r, this.handleRouting);
-    // } else {
-    //   console.log("nay");
-    // }
-  }
-  render() {
-    const { classes } = this.props;
-    const { cid } = this.state;
-    return (
-      <div onClick={this.props.onClick}>
-        <GoogleLogin
-          clientId={cid}
-          scope="https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/calendar"
-          //scope="https://www.googleapis.com/auth/gmail.readonly"
-          accessType="offline"
-          responseType="code"
-          render={renderProps => (
-            <Button
-              variant="contained"
-              key={`google-button`}
-              //style={{ backgroundColor: '#fff' }}
-              onClick={renderProps.onClick}
-              className={classes.socialButton}
-            >
-              Sign in with Google
-            </Button>
-          )}
-          buttonText="Login"
-          onSuccess={this.getToken}
-          onFailure={this.handleGoogleAuthFail}
-        />
-      </div>
-    );
-  }
+  const getToken = r => {
+    authAdmin(r, handleRouting);
+  };
+
+  const { classes } = props;
+
+  return (
+    <div onClick={props.onClick}>
+      <GoogleLogin
+        clientId={cid}
+        scope="https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/gmail.send https://www.googleapis.com/auth/calendar"
+        //scope="https://www.googleapis.com/auth/gmail.readonly"
+        accessType="offline"
+        responseType="code"
+        render={renderProps => (
+          <Button
+            variant="contained"
+            key={`google-button`}
+            //style={{ backgroundColor: '#fff' }}
+            onClick={renderProps.onClick}
+            className={classes.socialButton}
+          >
+            Sign in with Google
+          </Button>
+        )}
+        buttonText="Login"
+        onSuccess={getToken}
+        onFailure={handleGoogleAuthFail}
+      />
+    </div>
+  );
 }
 
 export default withRouter(withStyles(styles)(GoogleButton));
