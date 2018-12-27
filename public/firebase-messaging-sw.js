3fdc4f7b9f4529e2c175967bef3ef5ba66e353dd
shams mosowi <shams.mosowi@gmail.com>
2018-12-27 21:19:26 +1100

    disable notification sw

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,29 +1,29 @@
-// Give the service worker access to Firebase Messaging.
-// Note that you can only use Firebase Messaging here, other Firebase libraries
-// are not available in the service worker.
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
+// // Give the service worker access to Firebase Messaging.
+// // Note that you can only use Firebase Messaging here, other Firebase libraries
+// // are not available in the service worker.
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js');
+// importScripts('https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js');
 
-// Initialize the Firebase app in the service worker by passing in the
-// messagingSenderId.
-firebase.initializeApp({
-  messagingSenderId: '1045443129080',
-});
+// // Initialize the Firebase app in the service worker by passing in the
+// // messagingSenderId.
+// firebase.initializeApp({
+//   messagingSenderId: '1045443129080',
+// });
 
-// Retrieve an instance of Firebase Messaging so that it can handle background
-// messages.
-const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(payload => {
-  const title = payload.notification.title;
-  const body = payload.notification.body;
-  const options = {
-    body,
-    icon: 'https://admin2hat.firebaseapp.com/statics/inverted-favicon.png',
-  };
-  return self.registration.showNotification(title, options);
-});
-console.log('sw loaded messaging');
-// Handle incoming messages. Called when:
-// - a message is received while the app has focus
-// - the user clicks on an app notification created by a service worker
-//   `messaging.setBackgroundMessageHandler` handler.
+// // Retrieve an instance of Firebase Messaging so that it can handle background
+// // messages.
+// const messaging = firebase.messaging();
+// messaging.setBackgroundMessageHandler(payload => {
+//   const title = payload.notification.title;
+//   const body = payload.notification.body;
+//   const options = {
+//     body,
+//     icon: 'https://admin2hat.firebaseapp.com/statics/inverted-favicon.png',
+//   };
+//   return self.registration.showNotification(title, options);
+// });
+// console.log('sw loaded messaging');
+// // Handle incoming messages. Called when:
+// // - a message is received while the app has focus
+// // - the user clicks on an app notification created by a service worker
+// //   `messaging.setBackgroundMessageHandler` handler.
