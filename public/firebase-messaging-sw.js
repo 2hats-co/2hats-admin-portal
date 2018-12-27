57cbadfa6889163d1bb30978b24786cee1779ae2
shams mosowi <shams.mosowi@gmail.com>
2018-12-27 21:13:29 +1100

    fixing service worker attempt

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,3 @@
-import { options } from 'sw-toolbox';
-
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
