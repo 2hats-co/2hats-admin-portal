361c39c04790acd0f55af49a80e06da8f627779e
Sidney Alcantara <sidney@sidney.me>
2018-12-28 19:10:07 +1100

    campaign connections scrolly rolly

--- src/components/Marketing/LinkedinCampaigns/ConnectionRequest.js
+++ src/components/Marketing/LinkedinCampaigns/ConnectionRequest.js
@@ -3,7 +3,7 @@ import React from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
+import ListItem from '@material-ui/core/ListItem';
 
 import LinkedInIcon from '../../../assets/icons/LinkedIn';
 
@@ -11,18 +11,23 @@ import moment from 'moment';
 
 const styles = theme => ({
   root: {
-    '& + &': { marginTop: theme.spacing.unit },
+    // '& + &': { marginTop: theme.spacing.unit },
   },
   details: {
     position: 'relative',
-    paddingTop: theme.spacing.unit * 1.25,
   },
-  linkedInButton: {
-    marginLeft: -theme.spacing.unit * 1.75,
+  icon: {
+    marginLeft: -theme.spacing.unit * 0.375,
+    marginRight: theme.spacing.unit,
+    marginTop: theme.spacing.unit * 0.25,
+    color:
+      theme.palette.type === 'dark'
+        ? theme.palette.primary.darkText
+        : theme.palette.primary.main,
   },
   timestamp: {
     position: 'absolute',
-    top: theme.spacing.unit * 1.75,
+    top: theme.spacing.unit * 0.5,
     right: 0,
     opacity: 0.54,
   },
@@ -32,27 +37,31 @@ function ConnectionRequest(props) {
   const { classes, data } = props;
 
   return (
-    <Grid container className={classes.root}>
-      <Grid item>
-        <IconButton
-          className={classes.linkedInButton}
-          href={`https://linkedin.com${data.profileUrl}`}
-          color="primary"
-        >
-          <LinkedInIcon />
-        </IconButton>
-      </Grid>
+    <ListItem
+      button
+      component="a"
+      href={`https://linkedin.com${data.profileUrl}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Grid container className={classes.root}>
+        <Grid item>
+          <div className={classes.icon}>
+            <LinkedInIcon />
+          </div>
+        </Grid>
 
-      <Grid item xs className={classes.details}>
-        <Typography variant="subtitle1">{data.fullName}</Typography>
-        <Typography variant="body2" className={classes.timestamp}>
-          {moment.unix(data.createdAt.seconds).fromNow()}
-        </Typography>
+        <Grid item xs className={classes.details}>
+          <Typography variant="subtitle1">{data.fullName}</Typography>
+          <Typography variant="body2" className={classes.timestamp}>
+            {moment.unix(data.createdAt.seconds).fromNow()}
+          </Typography>
 
-        <Typography variant="body2">{data.tagLine}</Typography>
-        <Typography variant="body2">{data.currentPosition}</Typography>
+          <Typography variant="body2">{data.tagLine}</Typography>
+          <Typography variant="body2">{data.currentPosition}</Typography>
+        </Grid>
       </Grid>
-    </Grid>
+    </ListItem>
   );
 }
 
