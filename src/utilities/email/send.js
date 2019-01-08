3a530880e563cbade3327757be5509cb8f412e4b
Sidney Alcantara <sidney@sidney.me>
2019-01-08 15:17:39 +1100

    fix accept email not sending

--- src/utilities/email/send.js
+++ src/utilities/email/send.js
@@ -1,24 +1,25 @@
-import {firestore} from '../../store';
-import {COLLECTIONS} from '../../constants/firestore'
+import { firestore } from '../../store';
+import { COLLECTIONS } from '../../constants/firestore';
 
 /**
- * 
- * @param {{UID:string,email:string}} recipient 
- * @param {{UID:string,email:string}} sender 
- * @param {{subject:string,body:string}} email 
+ *
+ * @param {{UID:string,email:string}} recipient
+ * @param {{UID:string,email:string}} sender
+ * @param {{subject:string,body:string}} email
  */
-export const sendEmail = async (emailObject) => {
-    const {recipient, sender, email} = emailObject;
+export const sendEmail = async emailObject => {
+  const { recipient, sender, email } = emailObject;
 
-    const commDoc = {
-       UID: recipient.UID,
-       candidateEmail: recipient.email,
-       type: "outbox",
-       adminEmail: sender.email,
-       adminUID: sender.UID,
-       subject: email.subject,
-       body: email.body,
-       createdAt: new Date()
-   };
-   return await firestore.collection(COLLECTIONS.communications).add(commDoc);
-}
+  debugger;
+  const commDoc = {
+    UID: recipient.UID,
+    candidateEmail: recipient.email,
+    type: 'outbox',
+    adminEmail: sender.email,
+    adminUID: sender.UID,
+    subject: email.subject,
+    body: email.body,
+    createdAt: new Date(),
+  };
+  return await firestore.collection(COLLECTIONS.communications).add(commDoc);
+};
