2865d9a16c95d5dc9ab4781a059c0ea7eb870859
shams mosowi <shams.mosowi@gmail.com>
2019-01-20 13:38:10 +1100

    candidate conversation catagories

--- src/constants/conversationCategories.js
+++ src/constants/conversationCategories.js
@@ -1,10 +1,35 @@
-const conversationCategories = [
-  { value: 'futureNeed', label: '🤞🏼Future need🤞🏼' },
-  { value: 'generalCatchup', label: '☕️ catchup ☕️' },
-  { value: 'partnerships', label: '🤝 Partnership 🤝' },
-  { value: 'randoms', label: '🤡 Random 🤡' },
-  { value: 'client', label: '🤙🏼Client💪🏽' },
-  { value: 'hotLead', label: '🔥Hot Lead🔥' },
-];
+const conversationCategories = type => {
+  switch (type) {
+    case 'client':
+      return [
+        { value: 'futureNeed', label: '🤞🏼Future need🤞🏼' },
+        { value: 'generalCatchup', label: '☕️ catchup ☕️' },
+        { value: 'partnerships', label: '🤝 Partnership 🤝' },
+        { value: 'randoms', label: '🤡 Random 🤡' },
+        { value: 'client', label: '🤙🏼Client💪🏽' },
+        { value: 'hotLead', label: '🔥Hot Lead🔥' },
+      ];
+    case 'candidate':
+      return [
+        { value: 'pass', label: '👍🏻Pass👏🏽' },
+        { value: 'rejected', label: '🙅🏻‍Rejected🙅🏾‍' },
+        { value: 'TPA', label: '🏊🏻‍TP A🏊🏼‍' },
+        { value: 'TPB', label: '🏊🏻‍TP B🏊🏼‍' },
+      ];
+    default:
+      return [
+        { value: 'futureNeed', label: '🤞🏼Future need🤞🏼' },
+        { value: 'generalCatchup', label: '☕️ catchup ☕️' },
+        { value: 'partnerships', label: '🤝 Partnership 🤝' },
+        { value: 'randoms', label: '🤡 Random 🤡' },
+        { value: 'client', label: '🤙🏼Client💪🏽' },
+        { value: 'hotLead', label: '🔥Hot Lead🔥' },
+        { value: 'pass', label: '👍🏻Pass👏🏽' },
+        { value: 'rejected', label: '🙅🏻‍Rejected🙅🏾‍' },
+        { value: 'TPA', label: '🏊🏻‍TP A🏊🏼‍' },
+        { value: 'TPB', label: '🏊🏻‍TP B🏊🏼‍' },
+      ];
+  }
+};
 
 export default conversationCategories;
