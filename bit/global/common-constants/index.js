a75b4aef1bd215f5ed232d6119bd13580a4d5e6c
Sidney Alcantara <sidney@sidney.me>
2019-01-26 21:35:34 +1100

    update bit common-constants

--- bit/global/common-constants/index.js
+++ bit/global/common-constants/index.js
@@ -2,3 +2,6 @@ export { ASSESSMENT_CATEGORIES } from './assessmentCategories';
 export { SKILLS } from './skills';
 export { SUBMISSION_TYPES } from './submissionTypes';
 export { TRIGGERS } from './triggers';
+export { COLLECTIONS } from './collections';
+export { MOMENT_LOCALES, MOMENT_FORMATS } from './moment';
+export { ACTIVITY_LOG_TYPES, ACTIVITY_LOG_LABELS } from './activityLog';
---
+++ bit/global/common-constants/moment.js
@@ -0,0 +1,24 @@
+export const MOMENT_LOCALES = {
+  relativeTime: {
+    future: 'in %s',
+    past: '%s ago',
+    s: '1 s',
+    ss: '%d s',
+    m: '1 min',
+    mm: '%d min',
+    h: '1 h',
+    hh: '%d h',
+    d: '1 d',
+    dd: '%d d',
+    M: '1 M',
+    MM: '%d M',
+    y: '1 y',
+    yy: '%d y',
+  },
+};
+
+export const MOMENT_FORMATS = {
+  date: 'D/MM/YYYY',
+  time: 'h:mm a',
+  dateTime: 'D/MM/YYYY h:mm a',
+};
