d36f90c03b5b90a0b983b5121397f32b177963e7
Sidney Alcantara <sidney@sidney.me>
2019-01-26 12:39:07 +1100

    reset bit

--- src/constants/studentPortal/assessmentCategories.js
+++ src/constants/studentPortal/assessmentCategories.js
@@ -1,7 +1,6 @@
 const ASSESSMENT_CATEGORIES = [
   { label: 'Marketing', value: 'marketing' },
   { label: 'Sales', value: 'sales' },
-  { label: 'Copywriting', value: 'copywriting' },
   { label: 'Design', value: 'design' },
 ];
 
