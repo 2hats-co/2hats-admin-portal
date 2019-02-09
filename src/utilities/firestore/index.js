0421f2dd33c8ee4b1e403386e537e8db72eeec7d
Sidney Alcantara <sidney@sidney.me>
2019-02-09 18:08:19 +1100

    add updatedAt field to new docs

--- src/utilities/firestore/index.js
+++ src/utilities/firestore/index.js
@@ -66,6 +66,7 @@ export const createDoc = (collection, docData) =>
   firestore.collection(collection).add({
     ...docData,
     createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+    updatedAt: firebase.firestore.FieldValue.serverTimestamp(),
   });
 
 export const deleteDoc = (collection, docId) =>
