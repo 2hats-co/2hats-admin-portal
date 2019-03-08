1a5f31e5ba4c63fe7662cb38cdf39a4a01382277
shams mosowi <shams.mosowi@gmail.com>
2019-03-08 17:36:25 +1100

    youtube url, image uploader

--- src/components/Form/Fields/RichText.js
+++ src/components/Form/Fields/RichText.js
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import ReactQuill from 'react-quill';
+import Delta from 'quill-delta';
 import 'react-quill/dist/quill.snow.css';
 import { STYLES } from '@bit/sidney2hats.2hats.global.common-constants';
 
 import withStyles from '@material-ui/core/styles/withStyles';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
-
+import { asyncUploader } from '../../../utilities/firebaseStorage';
 const styles = theme => ({
   root: {
     margin: `${theme.spacing.unit}px 0`,
@@ -33,10 +34,44 @@ const styles = theme => ({
   },
 });
 
-const Text = props => {
+const RichText = props => {
   const { classes, label, name, placeholder, formikProps, validator } = props;
   const { values, errors, touched, setValues } = formikProps;
+  const [isUploading, setIsUploading] = useState(false);
+  const quillRef = useRef(null);
+  const now = new Date();
+  const imageHandler = () => {
+    const quill = quillRef.current.getEditor();
+    let fileInput = document.body.querySelector('input.ql-image[type=file]');
+    if (fileInput == null) {
+      fileInput = document.createElement('input');
+      fileInput.setAttribute('type', 'file');
+      fileInput.setAttribute(
+        'accept',
+        'image/png, image/gif, image/jpeg, image/bmp, image/x-icon'
+      );
+      fileInput.classList.add('ql-image');
+      fileInput.addEventListener('change', async () => {
+        if (fileInput.files != null && fileInput.files[0] != null) {
+          const ref = `quill-images/${now.getTime()}/${
+            fileInput.files[0].name
+          }`;
+          const downloadUrl = await asyncUploader(ref, fileInput.files[0]);
+          let range = quill.getSelection(true);
+          quill.updateContents(
+            new Delta()
+              .retain(range.index)
+              .delete(range.length)
+              .insert({ image: downloadUrl })
+          );
 
+          fileInput.value = '';
+        }
+      });
+      document.body.appendChild(fileInput);
+    }
+    fileInput.click();
+  };
   return (
     <Grid item xs={12} className={classes.root}>
       <Typography
@@ -44,27 +79,40 @@ const Text = props => {
         className={classes.sectionTitle}
         color={errors[name] && touched[name] ? 'error' : 'textSecondary'}
       >
-        {label}
+        {label}{' '}
+        {isUploading ? '    (please wait while we upload your image)' : ''}
       </Typography>
       <ReactQuill
         placeholder={placeholder}
         value={values[name]}
+        ref={quillRef}
         onChange={val => {
+          // const dataURLregEx = /"data:.*?"/;
+          // let matches = val.match(dataURLregEx);
+          // delete matches.index;
+          // delete matches.input;
+          // delete matches.groups;
+          // console.log(matches);
           setValues({ ...values, [name]: val });
         }}
         theme="snow"
         className={classes.quillEditor}
         preserveWhiteSpace
         modules={{
-          toolbar: [
-            ['bold', 'italic', 'underline'],
+          toolbar: {
+            container: [
+              ['bold', 'italic', 'underline'],
 
-            [{ header: 1 }, { header: 2 }],
-            [{ list: 'bullet' }, { list: 'ordered' }],
-            [{ indent: '-1' }, { indent: '+1' }],
-
-            ['link'],
-          ],
+              [{ header: 1 }, { header: 2 }],
+              [{ list: 'bullet' }, { list: 'ordered' }],
+              [{ indent: '-1' }, { indent: '+1' }],
+              ['blockquote', 'code-block', 'image', 'video'],
+              ['link'],
+            ],
+            handlers: {
+              image: imageHandler,
+            },
+          },
           clipboard: {
             matchVisual: false,
           },
@@ -75,4 +123,4 @@ const Text = props => {
   );
 };
 
-export default withStyles(styles)(Text);
+export default withStyles(styles)(RichText);
