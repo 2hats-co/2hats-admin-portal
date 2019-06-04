182cde746f5a628d47a39d016af05c7f2ddc71e5
Victor Chan <victorhchanc@gmail.com>
2019-06-04 14:28:04 +1000

    test container and adminFetch fn

--- src/containers/TestContainer.js
+++ src/containers/TestContainer.js
@@ -1,20 +1,23 @@
-import React from 'react';
-//import Button from '@material-ui/core/Button';
-import { callable, CLOUD_FUNCTIONS } from '../firebase/functions';
+import React, { useState, useEffect } from 'react';
+import adminFetch from '../utilities/adminAPI/adminFetch';
 
 function CallablesTestContainer(props) {
-  const { UID } = props;
-  console.log(UID);
-  const handleClick = () => {
-    console.log('youve clicked the button');
-    const inputData = {
-      query: {},
-      searchString: 'test2hats@gmail.com',
-      templateId: 'vMtGdsXYXRYMc9iYKgWQ',
-    };
-    const cb = o => console.log(o);
-    callable(CLOUD_FUNCTIONS.callablesSendTargeted, inputData, cb, cb);
-  };
-  return <button onClick={handleClick} />;
+  const [response, setResponse] = useState('');
+
+  useEffect(() => {
+    adminFetch('http://localhost:8000/protected', {
+      method: 'post',
+      body: JSON.stringify({ hello: 'sfaff' }),
+      headers: { 'Content-Type': 'application/json' },
+    })
+      .then(r => console.log(r))
+      .catch(r => console.log(r));
+  }, []);
+
+  return (
+    <>
+      <code>{response}</code>
+    </>
+  );
 }
 export default CallablesTestContainer;
