8092fa565ad2d1633997fe5bc93d372533ccc9df
Victor Chan <victorhchanc@gmail.com>
2019-06-06 10:29:14 +1000

    test

--- src/containers/TestContainer.js
+++ src/containers/TestContainer.js
@@ -1,23 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import adminFetch from '../utilities/adminAPI/adminFetch';
+import { functions } from '../store';
 
 function CallablesTestContainer(props) {
   const [response, setResponse] = useState('');
 
-  useEffect(() => {
-    adminFetch('http://localhost:8000/protected', {
-      method: 'post',
-      body: JSON.stringify({ hello: 'sfaff' }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then(r => console.log(r))
-      .catch(r => console.log(r));
-  }, []);
+  // useEffect(() => {
+  //   adminFetch('http://localhost:8000/protected', {
+  //     method: 'post',
+  //     body: JSON.stringify({ hello: 'sfaff' }),
+  //     headers: { 'Content-Type': 'application/json' },
+  //   })
+  //     .then(r => console.log(r))
+  //     .catch(r => console.log(r));
+  // }, []);
 
-  return (
-    <>
-      <code>{response}</code>
-    </>
-  );
+  //Prepare callable function
+  // const emailBlastActions = functions.httpsCallable(
+  //   'functions-emailBlastActions'
+  // );
+
+  // useEffect(() => {
+  //   async function fetchCallable() {
+  //     const data = {
+  //       action: 'create',
+  //       blastId: 'testBlastId',
+  //       args: {
+  //         query: 'victorchan.io',
+  //         blastsAt: new Date().toISOString(),
+  //         templateId: 'testTemplateId',
+  //         createdBy: '1231313123123',
+  //       },
+  //     };
+  //     const res = await emailBlastActions(data);
+  //     console.log(res);
+  //   }
+  //   fetchCallable();
+  // }, []);
+
+  return <>{/* <code>{response}</code> */}</>;
 }
 export default CallablesTestContainer;
